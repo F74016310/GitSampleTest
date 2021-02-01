e4440d9375be76406414151ad596ab1c3e789ad4
HarveyWang <108552030@cc.ncu.edu.tw>
2021-02-01 16:27:47 +0800

    3rd Version

--- GithubSample/src/testApp/MainApp.java
+++ GithubSample/src/testApp/MainApp.java
@@ -5,6 +5,8 @@ public class MainApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HelloWorld!");
+		//3rd version
+		System.out.println("3rd");
 	}
 
 }
